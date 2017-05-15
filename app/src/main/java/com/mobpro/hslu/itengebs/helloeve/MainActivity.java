39c76a3c7c6f76c6f6d0407d30773eb62f9850f8
krrli <carlaiten@gmail.com>
2017-05-15 18:53:10 +0200

    added ClickMe button and floatingActionButton for history

--- app/src/main/java/com/mobpro/hslu/itengebs/helloeve/MainActivity.java
+++ app/src/main/java/com/mobpro/hslu/itengebs/helloeve/MainActivity.java
@@ -2,12 +2,23 @@ package com.mobpro.hslu.itengebs.helloeve;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        ImageView clickMe = (ImageView) findViewById(R.id.clickMeImage);
+
+
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickMeClick(View view){
+        Toast.makeText(MainActivity.this, "hoiiii", Toast.LENGTH_LONG).show();
     }
 }
