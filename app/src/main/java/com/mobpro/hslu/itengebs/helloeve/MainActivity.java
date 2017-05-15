2c3e5a2c06be9b7b2c9db8e3f19c8be4f833ff1e
krrli <carlaiten@gmail.com>
2017-05-15 20:06:43 +0200

    changed colors and placement of bötten

--- app/src/main/java/com/mobpro/hslu/itengebs/helloeve/MainActivity.java
+++ app/src/main/java/com/mobpro/hslu/itengebs/helloeve/MainActivity.java
@@ -1,7 +1,9 @@
 package com.mobpro.hslu.itengebs.helloeve;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -11,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
 
         ImageView clickMe = (ImageView) findViewById(R.id.clickMeImage);
 
