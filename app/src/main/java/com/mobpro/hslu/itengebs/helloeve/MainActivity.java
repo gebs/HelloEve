3dd7f4996500e60b860d869c36723830615f108d
krrli <carlaiten@gmail.com>
2017-05-17 23:12:24 +0200

    rescued main activity from evil toolbar and menu

--- app/src/main/java/com/mobpro/hslu/itengebs/helloeve/MainActivity.java
+++ app/src/main/java/com/mobpro/hslu/itengebs/helloeve/MainActivity.java
@@ -1,16 +1,11 @@
 package com.mobpro.hslu.itengebs.helloeve;
 
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.widget.DrawerLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,55 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        final ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setHomeAsUpIndicator(R.mipmap.ic_menu);
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-
-        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
-        /*
-        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override public boolean onNavigationItemSelected(MenuItem menuItem) {
-                //Snackbar.make(content, menuItem.getTitle() + " pressed", Snackbar.LENGTH_LONG).show();
-                //menuItem.setChecked(true);
-                //drawerLayout.closeDrawers();
-                return true;
-            }
-        });
-        */
-
-        /*
-
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        final ActionBar actionBar = getSupportActionBar();
-
-        if (actionBar != null) {
-            actionBar.setHomeAsUpIndicator(R.mipmap.ic_menu);
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-
-        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        NavigationView view = (NavigationView) findViewById(R.id.navigation_view);
-        view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override public boolean onNavigationItemSelected(MenuItem menuItem) {
-                //Snackbar.make(content, menuItem.getTitle() + " pressed", Snackbar.LENGTH_LONG).show();
-                menuItem.setChecked(true);
-                drawerLayout.closeDrawers();
-                return true;
-            }
-        });
-*/
-
         setContentView(R.layout.activity_main);
     }
 
     public void onClickMeClick(View view){
         Toast.makeText(MainActivity.this, "hoiiii", Toast.LENGTH_LONG).show();
+    }
+
+    public void onHistoryFabClick(View view) {
+        Toast.makeText(MainActivity.this, "history", Toast.LENGTH_SHORT).show();
     }
 }
